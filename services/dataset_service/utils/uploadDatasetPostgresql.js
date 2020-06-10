aad8e8c80a460b2cd10fc1886c1f1d240613b187
teodorciripescu <teodor.ciripescu@gmail.com>
2020-06-10 21:17:12 +0300

    documentation & clear unnecessary libs

--- services/dataset_service/utils/uploadDatasetPostgresql.js
+++ services/dataset_service/utils/uploadDatasetPostgresql.js
@@ -69,7 +69,7 @@ async function uploadDatasetSync() {
     var lineNmb = 0;let query;
     var data = [];
     var s = fs.createReadStream('/home/asttrid/Documents/US_Accidents_Dec19.csv')
-        .pipe(es.split())
+        .pipe(es.split()) //impune stream-ului sa faca break la linii noi
         .pipe(es.mapSync(async function (line) {
             // punem pauza la stream (nu se continua citirea din fisier)
             s.pause();
@@ -144,16 +144,11 @@ function insertData(columns){
                 "$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42,$43,$44,$45,$46,$47,$48,$49)";
             conn.query(sql,columns[i])
                 .then(rows => {
-                    //console.log(columns[i]);
                     resolve(rows);
-                    //conn.end();
                 })
                 .catch(err => {
                     resolve(err);
-                    //handle query error
                 });
-            //const res = await conn.query(sql);
-            //resolve(res);
         }));
     }
     return Promise.all(promises);
